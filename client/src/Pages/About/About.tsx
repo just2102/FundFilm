9600e8ad81978d755515c2e17e36b4064b10e99a
Ilia Krasiuchenko <thesixteensaltines@gmail.com>
2025-08-19 01:58:11 +0700

    style: update About page styles and improve layout

--- client/src/Pages/About/About.tsx
+++ client/src/Pages/About/About.tsx
@@ -3,14 +3,14 @@ import styles from "./About.module.css";
 const About = () => {
   return (
     <div className={styles.about}>
-      <h1>About FundFilmDAO</h1>
+      <h1>About FundFilm</h1>
       <p>This service is intended for movie creators to raise funds for their projects.</p>
       <h2>OUR GOAL</h2>
       <p>is to connect moviemakers all over the world with their potential audience.</p>
       <h2>WE BELIEVE</h2>
       <p>that realization of creative nature is of utmost importance.</p>
       <h2>WE KNOW</h2>
-      <p>that making movies takes time, assets, and effort, and this is why this project exists.</p>
+      <p>that making movies takes time, assets, and effort, and this is why the project exists.</p>
 
       <div className={styles.meta}>
         <div className={styles.networksInfo}>
@@ -25,7 +25,7 @@ const About = () => {
             target='_blank'
             rel='noopener noreferrer'
           >
-            GitHub
+            Contribute
           </a>
         </p>
       </div>
